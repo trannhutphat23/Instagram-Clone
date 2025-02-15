2660323c8777467eef9a5b0ec5df212cd1bee08b
trannhutphat23 <trannhutphattv@gmail.com>
2025-02-15 22:06:25 +0700

    update comment model

--- src/main/java/com/example/InstagramClone/Model/Comment.java
+++ src/main/java/com/example/InstagramClone/Model/Comment.java
@@ -9,7 +9,6 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(
         name = "user_id",
@@ -17,7 +16,34 @@ public class Comment {
         foreignKey = @ForeignKey(name = "fk_user_comment")
     )
     private User_Profile user_profile;
-
     private String content;
+    public Comment() {
+    }
+    public Comment(User_Profile user_profile, String content) {
+        this.user_profile = user_profile;
+        this.content = content;
+    }
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User_Profile getUser_profile() {
+        return user_profile;
+    }
+
+    public void setUser_profile(User_Profile user_profile) {
+        this.user_profile = user_profile;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
